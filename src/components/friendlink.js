aaedde6cce6482c2d14cf1e939e0f107709c03fb
sanqian <linghui2019@gmail.com>
2020-04-21 15:25:16 +0800

    update friend link

--- src/components/friendlink.js
+++ src/components/friendlink.js
@@ -10,6 +10,7 @@ export default () => (
             <a href="https://like-gif.com/" target="_blank">gif收藏夹</a>
             <a href="http://www.yumojingshi.com" target="_blank">抛光机</a>
             <a href="https://www.51link.com/link-exchange?ruid=14086" target="_blank">交换友链</a>
+            <a href="http://85wangzhe.cn" target="_blank">泉州空压机</a>
         </div>
         </div>
     </div>
