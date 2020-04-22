f70723bb2e4620845a61ad32ec4ec17bddb93764
sanqian <linghui2019@gmail.com>
2020-04-22 14:07:55 +0800

    update friend link

--- src/components/friendlink.js
+++ src/components/friendlink.js
@@ -9,6 +9,7 @@ export default () => (
             <a href="https://www.52beautys.com/" target="_blank">妹子图片</a>
             <a href="https://like-gif.com/" target="_blank">gif收藏夹</a>
             <a href="http://www.yumojingshi.com" target="_blank">抛光机</a>
+            <a href="http://gjcblsn.cn" target="_blank">丝瓜视频苹果版app</a>
             <a href="https://www.51link.com/link-exchange?ruid=14086" target="_blank">交换友链</a>
             <a href="http://85wangzhe.cn" target="_blank">泉州空压机</a>
         </div>
